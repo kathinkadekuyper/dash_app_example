8085101d40a5b60496feb27a589349fa9a49ad02
Kathinka <kathinka.dekuijper@alumni.esade.edu>
2018-12-03 15:21:34 +0100

    First commit

---
+++ app.py
@@ -0,0 +1,17 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
+import pandas as pd
+
+app = dash.Dash(__name__)
+server = app.server
+app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
+
+
